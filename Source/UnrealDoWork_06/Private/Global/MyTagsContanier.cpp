3740a55bec6a61b0617b3124f11e50b741aa1bd7
gimunkwon <gimon71@gmail.com>
2026-06-08 07:14:57 +0900

    [Feature] 태그정의

--- Source/UnrealDoWork_06/Private/Global/MyTagsContanier.cpp
+++ Source/UnrealDoWork_06/Private/Global/MyTagsContanier.cpp
@@ -1,2 +1,6 @@
 #include "UnrealDoWork_06/Public/Global/MyTagsContanier.h"
 
+namespace ObjectPoolTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Object_Pooling, "Object.Pooling")
+}
--- Source/UnrealDoWork_06/Public/Global/MyTagsContanier.h
+++ Source/UnrealDoWork_06/Public/Global/MyTagsContanier.h
@@ -1,4 +1,9 @@
 #pragma once
 
-#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace ObjectPoolTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Object_Pooling)
+}
 
